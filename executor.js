7ad0806642f1832aa9e3c3e0625e3bb0b77af915
Derek Yap <zangzi@gmail.com>
2014-04-02 22:13:54 -0400

    Added working firefox support. However it requires selenium to be started manually. If java isn't install properly or isn't running properly in the command shell then this will be the case. Instructions will be necessary even though it isn't hard to start. Also I am worried about IE as the schools will probably make sure it is hard to run.

--- executor.js
+++ executor.js
@@ -15,6 +15,7 @@ var version = '0.0.1';
 var util = require('util');
 var fs = require("fs");
 var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var os = require('os');
 
 // - BrowserMob Proxy
@@ -59,8 +60,10 @@ var logger = new (winston.Logger)({
 // ----------------------------
 // GLOBALS:
 // ----------------------------
-var SELENIUM_LINUX_DEFAULT_PATH = './selenium/selenium-server-standalone-2.41.0.jar';
-var SELENIUM_WINDOWS_DEFAULT_PATH = '.\\selenium\\selenium-server-standalone-2.41.0.jar';
+var QUIT_TIMEOUT = 30000; // 30 seconds.
+
+var SELENIUM_LINUX_DEFAULT_PATH = './selenium-server-standalone-2.41.0.jar';
+var SELENIUM_WINDOWS_DEFAULT_PATH = '.\\selenium-server-standalone-2.41.0.jar';
 var BROWSERMOB_LINUX_DEFAULT_PATH = './browsermob/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy';
 var BROWSERMOB_WINDOWS_DEFAULT_PATH = '.\\browsermob\\browsermob-proxy-2.0-beta-9\\bin\\browsermob-proxy.bat';
 
@@ -84,16 +87,19 @@ var AMQP_CH = null;
 
 var SCHOOLS = ['navy', 'marines', 'army', 'airforce', 'coastguard', 'nps', 'rmc'];
 var MACHINES = ['winxp', 'win7', 'ubuntu', 'centos'];
+var BROWSERS = ['ie', 'firefox'];
+var EXECUTOR = '';
 
 // ----------------------------
 // Commander:
 // ----------------------------
 cmdr.description('CDX GreyCell Executor -- Executes tasking from central server')
-	.option('-u, --school <school>', 'Please specify school or group. Allowed list: '+SCHOOLS.join(', '))
+	.option('-s, --school <school>', 'Please specify school or group. Allowed list: '+SCHOOLS.join(', '))
 	.option('-m, --machine <machine>', 'Please specify machine. Allowed list: '+MACHINES.join(', '))
+	.option('-b, --browser <browser>', 'Please specify browser. Allowed list: '+BROWSERS.join(', '))
 	.option('-ah, --amqp_host <server name or IP>', 'AMQP Server Hostname', AMQP_IP)
 	.option('-ap, --amqp_port <port number>', 'AMQP Server Port', parseInt, AMQP_PORT)
-	.option('-s,  --sel <path>', 'Path to Selenium JAR File', verifySeleniumPath, SELENIUM_WINDOWS_DEFAULT_PATH)
+	.option('-sx,  --sel <path>', 'Path to Selenium JAR File', verifySeleniumPath, SELENIUM_WINDOWS_DEFAULT_PATH)
 	.option('-sh, --sel_hostname <hostname>', 'Selenium execution hostname', SELENIUM_HOST)
 	.option('-sp, --sel_port <n>', 'Selenium execution port number', parseInt, SELENIUM_PORT)
 	.option('-px, --proxy_path [path]', 'Use Browsermob Proxy', verifyBrowserMobPath, BROWSERMOB_WINDOWS_DEFAULT_PATH)
@@ -125,7 +131,7 @@ function verifySeleniumPath(inputPath) {
 		selenium_path = inputPath;
 	} else if (os.platform() === 'linux') {
 		selenium_path = SELENIUM_LINUX_DEFAULT_PATH;
-	} else if (os.platform() === 'windows') {
+	} else if (os.platform() === 'win32') {
 		selenium_path = SELENIUM_WINDOWS_DEFAULT_PATH;
 	}
 	if (fs.existsSync(selenium_path)){
@@ -156,7 +162,7 @@ var start = function () {
 		}
 	}
 	
-	// Error out if we don't get a school or group:
+	// Error out if we don't get a machine:
 	if (!_.isString(cmdr.machine)) {
 		logger.error('Machine must be provided. Choices: '+MACHINES.join(', '));
 		process.exit(1); // May need to kick out.
@@ -168,12 +174,27 @@ var start = function () {
 		}
 	}
 	
+	// Error out if we don't get a machine:
+	if (!_.isString(cmdr.browser)) {
+		logger.error('Browser must be provided. Choices: '+BROWSERS.join(', '));
+		process.exit(1); // May need to kick out.
+	} else {
+		var choosenSchool = _.indexOf(BROWSERS, cmdr.browser);
+		if ( choosenSchool == -1) {
+			logger.error('Browser must be provided. Choices: '+BROWSERS.join(', '));
+			process.exit(1);
+		}
+	}
+	
 	logger.info('OS Platform: ' + os.platform());
 	logger.info('OS Release: '+os.release());
 	logger.info('OS Type: '+os.type());
 	logger.info('OS Hostname: '+ os.hostname());
+	logger.info('Process Env: '+ util.inspect(process.env.Path));
 	logger.info('School: '+cmdr.school);
-	logger.info('Machine: '+cmdr.machine);	
+	logger.info('Machine: '+cmdr.machine);
+	EXECUTOR = cmdr.school + '.' + cmdr.machine;
+	logger.info('Executor: '+EXECUTOR);
 	logger.info('AMQP Server: ' + cmdr.amqp_host);
 	logger.info('AMQP Port: ' + cmdr.amqp_port);
 	logger.info('Selenium Path: '+cmdr.sel);
@@ -182,7 +203,7 @@ var start = function () {
 	logger.info('Proxy Path: '+cmdr.proxy_path);
 	logger.info('Proxy Hostname: '+cmdr.proxy_hostname);
 	logger.info('Proxy Port: '+cmdr.proxy_port);
-	
+
 	// Resolver:
 	var executor = cmdr.school + '.' + cmdr.machine;
 	startDefer.resolve(executor);
@@ -243,50 +264,95 @@ var start = function () {
 
 //SELENIUM_SERVER = new DriverService('IEDriverServer.exe', {port: 4444});
 
+var sendResult = function(amqpmsg, taskID, output, startTime) {
+	var result = {};
+	result.taskID = taskID;
+	result.output = output;
+	result.executor = EXECUTOR;
+	result.os_platform = os.platform();
+	result.os_release = os.release();
+	result.startTime = startTime;
+	result.endTime = Date.now();
+	result.elapsedTime = result.endTime - result.startTime;
+	
+	logger.debug('sendResult :: Result:\n'+util.inspect(result));
+	
+	var message = JSON.stringify(result);
+	AMQP_CH.publish(AMQP_RESULTS_EXCHANGE, AMQP_RESULTS_ROUTING_KEY, new Buffer(message));
+	logger.debug("sendResult :: RESULT Sent!! :: %s:'%s'", AMQP_RESULTS_ROUTING_KEY, message);
+	AMQP_CH.ack(amqpmsg);
+};
+
 var handleTask = function(msg) {
-	logger.debug(" [x] %s:'%s'",
+	logger.debug("handleTask :: [x] %s:'%s'",
 				msg.fields.routingKey,
 				msg.content.toString());
 	
+	var startTime = Date.now();
 	var taskingObj = JSON.parse(msg.content.toString());
-	logger.debug('Tasking Object:\n'+util.inspect(taskingObj));
+	logger.debug('handleTask :: Tasking Object:\n'+util.inspect(taskingObj));
 	if (taskingObj.cmd === 'execute_url') {
+		logger.debug('handleTask :: execute_url :: Executing task...');
 		var driver = SELENIUM_DRIVER;
 		driver.get(taskingObj.url);
 		driver.sleep(taskingObj.workTime);
 		driver.getTitle().then(function(title) {
-			var result = {};
-			result.taskID = taskingObj.taskID;
-			result.output = title;
-			result.executor = cmdr.school + '.' + cmdr.machine;
-			result.os_platform = os.platform();
-			result.os_release = os.release();
-			
-			logger.debug('Result:\n'+util.inspect(result));
-			
-			var message = JSON.stringify(result);
-			AMQP_CH.publish(AMQP_RESULTS_EXCHANGE, AMQP_RESULTS_ROUTING_KEY, new Buffer(message));
-			logger.debug(" RESULT!! Sent %s:'%s'", AMQP_RESULTS_ROUTING_KEY, message);
-			AMQP_CH.ack(msg);
+			logger.debug('handleTask :: execute_url :: Sending result...');
+			sendResult(msg, taskingObj.taskID, title, startTime);
 		});
+	} else if (taskingObj.cmd === 'pause') {
+		logger.info('handleTask :: Pausing for '+taskingObj.workTime+'ms. Will send message once timeout is reached.');
+		setTimeout(function () {
+			logger.info('handleTask :: Timeout Reached. Sending message.');
+			sendResult(msg, taskingObj.taskID, 'paused', startTime);
+		}, taskingObj.workTime);
+	} else if (taskingObj.cmd === 'quit') {
+		logger.info('handleTask :: Quiting in '+QUIT_TIMEOUT+'ms. Will send message once quit timeout is reached.');
+		setTimeout(function () {
+			logger.info('handleTask :: Quiting Timeout Reached. Sending message.');
+			sendResult(msg, taskingObj.taskID, 'quit', startTime);
+			setTimeout(function () {
+				logger.info('handleTask :: Quiting...');
+				process.kill(process.pid, 'SIGTERM');
+			}, QUIT_TIMEOUT);
+		}, QUIT_TIMEOUT);
 	}
 };
 
 start().then(function () {
 	// Start Driver:
-	// IE Setup:
-	var portStr = '--port='+cmdr.sel_port;
-	spawn('IEDriverServer.exe', [portStr], {detached: true});
-	var serverAddress = 'http://localhost:'+cmdr.sel_port;
-	var driver = new webdriver.Builder().
-		usingServer(serverAddress).
-	    withCapabilities({
-			browserName: 'internet explorer',
-			platform: 'WINDOWS',
-			INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS: true
-	    }).
-	    build();
-	
+	var driver = null;
+	if (os.platform() === 'win32') {
+		if (cmdr.browser === 'ie') {
+			// IE Setup:
+			var portStr = '--port='+cmdr.sel_port;
+			spawn('IEDriverServer.exe', [portStr]);
+			var serverAddress = 'http://localhost:'+cmdr.sel_port;
+			var driver = new webdriver.Builder().
+				usingServer(serverAddress).
+			    withCapabilities({
+					browserName: 'internet explorer',
+					platform: 'WINDOWS',
+					INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS: true
+			    }).
+			    build();
+		} else if (cmdr.browser === 'firefox') {
+			var serverAddress = 'http://localhost:'+cmdr.sel_port+'/wd/hub';
+			var driver = new webdriver.Builder().
+				usingServer(serverAddress).
+			    withCapabilities(webdriver.Capabilities.firefox()).
+			    build();
+		}
+	} else if (os.platform() === 'linux') {
+		if (cmdr.browser === 'firefox') {
+			var serverAddress = 'http://127.0.0.1:'+cmdr.sel_port+'/wd/hub';
+			var driver = new webdriver.Builder().
+				usingServer(serverAddress).
+			    withCapabilities(webdriver.Capabilities.firefox()).
+			    build();
+		}
+	}
+
 	return driver;
 }).then(function (driver) {
 	SELENIUM_DRIVER = driver;
